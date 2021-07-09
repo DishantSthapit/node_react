d910f80eb2278405a12b53baad1b6295f80e3a42
dishant <dishantsthapit@gmail.com>
2021-07-09 16:51:16 +1000

    adding nodemon package and adding heper function

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-console.log(global.process)
+const { sum } = require('./helper')
+const http = require('http') //Inbuilt core node js module
+
+
+// Creating a server using http server
+const server =  http.createServer((req,res) => { 
+    res.end("Hello world from node js")
+})
+
+// Listen to that server in port 3000
+server.listen(3000);
+
+
+const total = sum(2,3)
+console.log(total)
--- helper.js
+++ helper.js
@@ -1,7 +1,2 @@
-function sum (a,b) {
-    return a+b;
-}
+exports.sum = (a,b) => a+b;
 
-module.exports = {
-    sum
-}
