4693cb22cb0bd95e4006a86ed2ff2d1fa4826ab5
dishant <dishantsthapit@gmail.com>
2021-07-27 20:34:35 +1000

    code cleanup

--- routes/post.js
+++ routes/post.js
@@ -1,10 +1,12 @@
 const express = require('express')
-const postController = require('../controllers/post')
-
+const {getPosts,createPosts} = require('../controllers/post')
+const validator = require('../validator')
 // Router is a method in express which is used to route
 const router = express.Router();
 
-router.get("/", postController.getPosts);
-router.post("/post", postController.createPosts);
+router.get("/", getPosts);
+
+//validation before creating a post, if it passes it goes to next middleware i.e. postController.createPosts
+router.post("/post", validator.createPostValidator, createPosts);
 
 module.exports = router
